64b6e3e193f43023f1aafd5986a126f16478aab5
Rosana <65780925+rosaninha@users.noreply.github.com>
2020-08-31 18:37:15 -0400

    create
    tryout

---
+++ Chapter02/frmMain.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Windows.Forms;
+public class frmMain : Form
+{
+    private Label label1;
+    #region Windows Code
+    private void InitializeComponent()
+    {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // frmMain
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMain";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+    #endregion
+    public frmMain()
+    {
+        InitializeComponent();
+    }
+    [STAThread]
+    public static void Main()
+    {
+        frmMain main = new frmMain();
+        Application.Run(main);
+    }
+
+    private void frmMain_Load(object sender, EventArgs e)
+    {
+
+    }
+}
