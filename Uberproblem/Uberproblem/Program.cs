17b9fc1c9f6e1487121d61b254a19abe89d3cfd2
Rosana <65780925+rosaninha@users.noreply.github.com>
2020-09-01 21:13:27 -0400

    createuberprob
    new

---
+++ Uberproblem/Uberproblem/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UberProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 1;
+
+            Console.WriteLine("Hello Uber");
+            int[] arrInput = { 3, 2, 1 };
+            int[] arrOutput = new int[arrInput.Length];
+
+            for (int i = 0; i < arrInput.Length; i++)
+            {
+                result = 1;
+
+                for (int j = 0; j < arrInput.Length; j++)
+                {
+                    if (i != j)
+                    {
+
+                        result = (arrInput[j] * result);
+                    }
+
+                }
+                arrOutput[i] = result;
+
+                Console.WriteLine(result);
+            }
+
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+    }
+}
